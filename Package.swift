cb98f6839ecb6be7e6837f798c4f7cfb6672723b
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-13 22:22:07 +0100

    Update package name and add project dependencies

--- Package.swift
+++ Package.swift
@@ -3,24 +3,25 @@
 import PackageDescription
 
 let package = Package(
-  name: "template-service",
+  name: "listing-service",
   products: [
     .library(name: "App", targets: ["App"]),
     .executable(name: "Run", targets: ["Run"])
   ],
   dependencies: [
     .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.3.0")),
-    .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0"))
+    .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
+    .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.1.0")),
+    .package(url: "https://github.com/goRestart/core-service.git", .branch("develop"))
     ],
   targets: [
-    .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
+    .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "CoreService"],
             exclude: [
               "Config",
               "Public",
               "Resources",
               ]),
-    .target(name: "Run", dependencies: ["App"]),
-    .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    .target(name: "Run", dependencies: ["App"])
   ]
 )
 
