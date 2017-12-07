3ea94e03b5c9f1c67a1ed71b75936d44f70c07c3
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-07 20:55:09 +0100

    Wire dependencies

--- Sources/App/Core/Infrastructure/Assembly/Assembly+Mapper.swift
+++ Sources/App/Core/Infrastructure/Assembly/Assembly+Mapper.swift
@@ -3,11 +3,16 @@ import CoreService
 extension Assembly {
   var productMapper: ProductMapper {
     return ProductMapper(
-      priceMapper: priceMapper
+      priceMapper: priceMapper,
+      imageMapper: imageMapper
     )
   }
   
   private var priceMapper: PriceMapper {
     return PriceMapper()
+  }
+  
+  private var imageMapper: ImageMapper {
+    return ImageMapper()
   }
 }
--- Sources/App/Core/Infrastructure/Assembly/Assembly+Task.swift
+++ Sources/App/Core/Infrastructure/Assembly/Assembly+Task.swift
@@ -1,11 +1,13 @@
 import CoreService
 import game_service_api
+import image_service_api
 
 extension Assembly {
   var addProductTask: AddProductTask {
     return AddProductTask(
       getGameById: getGameById,
-      getGameConsoleById: getGameConsoleById
+      getGameConsoleById: getGameConsoleById,
+      getImagesByIds: getImagesByIds
     )
   }
   
@@ -15,5 +17,9 @@ extension Assembly {
   
   private var getGameConsoleById: GetGameConsoleById {
     return GetGameConsoleById()
+  }
+  
+  private var getImagesByIds: GetImagesByIds {
+    return GetImagesByIds()
   }
 }
