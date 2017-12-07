f9d0d3e8548c54718598a32a1cc2464e1943ea90
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-07 14:47:29 +0100

    Update product controller to forward calls and do error handling

--- Sources/App/API/Controller/ProductController.swift
+++ Sources/App/API/Controller/ProductController.swift
@@ -1,51 +1,26 @@
 import HTTP
 import CoreService
-import Foundation
-
-private struct Parameter {
-  static let gameId = "game_id"
-  static let gameConsoleId = "game_console_id"
-  static let description = "description"
-  static let imageIds = "image_ids"
-  
-  struct Price {
-    static let root = "price"
-    static let value = "value"
-    static let currencyLocale = "currency_locale"
-  }
-}
 
 struct ProductController {
   
+  private let addProduct: AddProduct
+  
+  init(addProduct: AddProduct) {
+    self.addProduct = addProduct
+  }
+  
   func addProduct(with request: Request) throws -> ResponseRepresentable {
-    guard let json = request.json else {
+    do {
+      let addProductRequest = try request.extract()
+      return try addProduct
+        .execute(with: addProductRequest)
+        .makeResponse(status: .created)
+    } catch AddProductRequestError.missingParameters {
       return Response.missingParameters
+    } catch ProductError.invalidGameConsoleId {
+      return Response.invalidGameConsoleId
+    } catch ProductError.invalidGameId {
+      return Response.invalidGameId
     }
-    guard let gameId = json[Parameter.gameId]?.string,
-      let gameConsoleId = json[Parameter.gameConsoleId]?.string,
-      let description = json[Parameter.description]?.string,
-      let imageIds = json[Parameter.imageIds]?.array,
-      let productPrice = json[Parameter.Price.root]?.object,
-      let priceValue = productPrice[Parameter.Price.value]?.double,
-      let priceCurrencyLocale = productPrice[Parameter.Price.currencyLocale]?.string,
-      !description.trim().isEmpty, !imageIds.isEmpty else {
-        return Response.missingParameters
-    }
-    
-    let price = Price(
-      value: priceValue,
-      locale: Locale(identifier: priceCurrencyLocale)
-    )
-    let images = imageIds.flatMap { $0.string }
-
-    let addProductRequest = AddProductRequest(
-      gameId: gameId,
-      gameConsoleId: gameConsoleId,
-      description: description,
-      imageIds: images,
-      price: price
-    )
-    
-    return "{add product request: \(addProductRequest)}"
   }
 }
